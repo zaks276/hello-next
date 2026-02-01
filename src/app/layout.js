0269266ef72b6d4a3a32c1f722ae76dfec397f53
Mohammed Zakaria Khan <zaks276@gmail.com>
2026-02-01 17:33:05 +0200

    Completed Exercise

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,11 +3,15 @@ import React from 'react';
 import './styles.css';
 
 function RootLayout({ children }) {
+  const timeStamp = new Date().toLocaleString();
   return (
     <html lang="en">
       <body>
         {children}
-
+        <footer>
+        Page rendered on {' '}
+        {timeStamp}
+        </footer>
       </body>
     </html>
   );
